0806ad072305c6716486b42d57adff225b970f9f
harbargerdev <codeamber0621@pm.me>
2025-05-05 20:33:33 -0400

    Adding some simple docs on the purpose of the files

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+"""
+_summary_
+Simple module to validate a container image using the `validate_container` package.
+This module provides a command-line interface to validate a container image.
+"""
 from validate_container import main
 
 # Entry point for standalone execution
--- validate_container.py
+++ validate_container.py
@@ -1,3 +1,8 @@
+"""
+_summary_
+Simple module to validate a container image using the `validate_container` package.
+This module provides a command-line interface to validate a container image.
+"""
 def main():
     print("Dev container validation test successful!")
 
